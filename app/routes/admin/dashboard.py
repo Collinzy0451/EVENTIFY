2bf2685155d238c71cdff127031b91ce9db76549
Collinzy0451 <jacobcollinzy6@gmail.com>
2024-06-21 10:47:16 +0100

    admin feautures

--- app/routes/admin/dashboard.py
+++ app/routes/admin/dashboard.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
+from flask_login import login_required
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('/admin/dashboard.html')
