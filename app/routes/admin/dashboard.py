9aa800fe4e3cdd77198621e7e81a0f9daf7e6806
Collinzy0451 <jacobcollinzy6@gmail.com>
2024-06-24 20:34:34 +0100

    updating routes

--- app/routes/admin/dashboard.py
+++ app/routes/admin/dashboard.py
@@ -1,8 +1,34 @@
 from flask import render_template
 from app import app
 from flask_login import login_required
-
+from app.models.admin import Event
+from app.models.user import Registration
+ 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('/admin/dashboard.html')
+
+    #Retrieve existing_events from the database.
+    existing_event = Event.query.first()
+
+    if existing_event:
+        total_tickets = existing_event.total_tickets
+
+        #count the number of registered users.
+        tickets_used = Registration.query.count()
+
+        #calculate the number of tickets left
+        tickets_left = total_tickets - tickets_used
+
+        #retrieve all user registration
+        registration = Registration.query.all()
+
+        return render_template(
+            'admin/dashboard.html',
+            total_tickets=total_tickets,
+            tickets_used=tickets_used,
+            tickets_left=tickets_left,
+            existing_event=existing_event
+        )
+
+    return render_template('admin/dashboard.html')
